a24593c9da47f7e7fb19d01de1b1223605c13e49
xiaodeng <2316288030@qq.com>
2023-03-16 14:42:22 +0800

    init

---
+++ src/Db/Exception/DbEventException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Yng\Db\Exception;
+
+/**
+ * Db事件异常
+ */
+class DbEventException extends DbException
+{
+}
---
+++ src/Db/Exception/ModelEventException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Yng\Db\Exception;
+
+/**
+ * 模型事件异常
+ */
+class ModelEventException extends DbException
+{
+}
